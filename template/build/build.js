fef1f3fa9c6a5d780efdb0bbb5414902fcd3a15f
孙正强 <794176533@qq.com>
2019-02-20 16:03:47 +0800

    修改meta文件配置

--- template/build/build.js
+++ template/build/build.js
@@ -2,7 +2,7 @@
 require('./check-versions')()
 
 const ora = require('ora')
-const opn=require('opn')
+const opn = require('opn')
 const rm = require('rimraf')
 const path = require('path')
 const chalk = require('chalk')
@@ -13,58 +13,58 @@ var connect = require('connect')
 var serveStatic = require('serve-static')
 
 const spinner = ora(
-  'building for ' + process.env.env_config + ' environment...'
+    'building for ' + process.env.env_config + ' environment...'
 )
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
-  webpack(webpackConfig, (err, stats) => {
-    spinner.stop()
     if (err) throw err
-    process.stdout.write(
-      stats.toString({
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false
-      }) + '\n\n'
-    )
-
-    if (stats.hasErrors()) {
-      console.log(chalk.red(' Build failed with errors.\n'))
-      process.exit(1)
-    }
-
-    console.log(chalk.cyan(' Build complete.\n'))
-    console.log(
-      chalk.yellow(
-        ' Tip: built files are meant to be served over an HTTP server.\n' +
-          " Opening index.html over file:// won't work.\n"
-      )
-    )
-
-    if (process.env.npm_config_preview) {
-      const port = 9526
-      const host = 'http://localhost:' + port
-      const basePath = config.build.assetsPublicPath
-      const app = connect()
-
-      app.use(
-        basePath,
-        serveStatic('./dist', {
-          index: ['index.html', '/']
-        })
-      )
-
-      app.listen(port, function() {
-        const url=`http://localhost:${port}${basePath}`;
-        opn(url);
-        console.log(
-          chalk.green(`> Listening at  ${url}`)
+    webpack(webpackConfig, (err, stats) => {
+        spinner.stop()
+        if (err) throw err
+        process.stdout.write(
+            stats.toString({
+                colors: true,
+                modules: false,
+                children: false,
+                chunks: false,
+                chunkModules: false
+            }) + '\n\n'
         )
-      })
-    }
-  })
+
+        if (stats.hasErrors()) {
+            console.log(chalk.red(' Build failed with errors.\n'))
+            process.exit(1)
+        }
+
+        console.log(chalk.cyan(' Build complete.\n'))
+        console.log(
+            chalk.yellow(
+                ' Tip: built files are meant to be served over an HTTP server.\n' +
+                " Opening index.html over file:// won't work.\n"
+            )
+        )
+
+        if (process.env.npm_config_preview) {
+            const port = config.dev.port
+            const host = config.dev.host
+            const basePath = config.build.assetsPublicPath
+            const app = connect()
+
+            app.use(
+                basePath,
+                serveStatic('./dist', {
+                    index: ['index.html', '/']
+                })
+            )
+
+            app.listen(port, function() {
+                const url = `http://${host}:${port}${basePath}`;
+                opn(url);
+                console.log(
+                    chalk.green(`> Listening at  ${url}`)
+                )
+            })
+        }
+    })
 })
